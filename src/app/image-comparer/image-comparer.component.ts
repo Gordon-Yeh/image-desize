37e71189c33d3e3054e2f55cd92a244e7acc693c
Gordon Yeh <gordon.yeh.dev@gmail.com>
2020-08-11 22:32:35 -0700

    display file size

--- src/app/image-comparer/image-comparer.component.ts
+++ src/app/image-comparer/image-comparer.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'image-comparer',
@@ -7,11 +8,14 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./image-comparer.component.scss']
 })
 export class ImageComparerComponent implements OnInit {
-  quality = new FormControl(100);
+  quality = new FormControl(85);
+  imgPathField = new FormControl();
+  orig : File;
+  cmprsd : File;
 
-  constructor() { }
+  constructor(private ref: ChangeDetectorRef) { }
 
-  loadAndDrawImage(canvas : HTMLCanvasElement, url : string, callback = () => {}) : void {
+  drawImage(canvas : HTMLCanvasElement, url : string, callback = () => {}) : void {
     var parent = canvas.parentElement;
     let img = new Image();
     img.src = url;
@@ -35,15 +39,80 @@ export class ImageComparerComponent implements OnInit {
     }
   }
 
-  ngOnInit() {
-    var jQuality = 0.85;
+  getOGFileSize() : number {
+    return this.orig ? this.orig.size : -1;
+  }
+
+  getCPFileSize() : number {
+    return this.cmprsd ? this.cmprsd.size : -1;
+  }
+
+  toSizeString(bytes : number, unit : 'B' | 'KB' | 'MB' | 'GB') : string {
+    var denom = {
+      'B': 1,
+      'KB': 1_000,
+      'MB': 1_000_000,
+      'GB': 1_000_000_000
+    };
+
+    return `${bytes / denom[unit]} ${unit}`;
+  }
+
+  drawBlob(canvas : HTMLCanvasElement, blob : Blob, callback = () => {}) : void {
+    // TODO: check that blob is an image file
+    var bUrl = URL.createObjectURL(blob);
+    this.drawImage(canvas, bUrl, () => {
+      callback();
+      URL.revokeObjectURL(bUrl);
+    });
+  }
+
+  handleDownload() {
+    var a = document.createElement('a');
+    a.download = 'true';
+    var bUrl = URL.createObjectURL(this.cmprsd);
+    a.href = bUrl;
+    a.click();
+    URL.revokeObjectURL(bUrl);
+    a.remove();
+  }
+
+  handleQualityChange() {
+    console.log('handleQualityChange', this.quality.value);
     var before = document.getElementById('before') as HTMLCanvasElement;
     var after = document.getElementById('after') as HTMLCanvasElement;
-    this.loadAndDrawImage(before, '/assets/profile-1.jpg', () => {
-      before.toBlob((blob) => {
-        var cUrl = URL.createObjectURL(blob);
-        this.loadAndDrawImage(after, cUrl);
-      }, 'image/jpeg', jQuality);
-    });
+    // apply JPEG compressing using 
+    before.toBlob((b : File) => {
+      console.log(b);
+      this.cmprsd = b;
+      this.drawBlob(after, b);
+      // a fix for file size display not being updated properly
+      this.ref.detectChanges();
+    }, 'image/jpeg', this.quality.value / 100);
+  }
+
+  imgFileSelectHandler() {
+    var fileInput = document.getElementById('file-selector') as HTMLFormElement;
+    var file : File = fileInput.files[0];
+    console.log(file);
+    if (file.type !== 'image/jpeg') {
+      alert(`${file.name} is not a JPEG file`);
+      return;
+    }
+    this.orig = file;
+    var before = document.getElementById('before') as HTMLCanvasElement;
+    this.drawBlob(before, file, () => { this.handleQualityChange() });
+  }
+
+  ngOnInit() {
+    // resize images appropriately if view size changes
+    window.addEventListener('resize', () => {
+      if (this.orig) {
+        var before = document.getElementById('before') as HTMLCanvasElement;
+        var after = document.getElementById('after') as HTMLCanvasElement;
+        this.drawBlob(before, this.orig);
+        this.drawBlob(after, this.cmprsd);
+      }
+    })
   }
 }
