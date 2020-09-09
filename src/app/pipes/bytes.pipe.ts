d71770e690e3fbe3a8022f01b422d2fc0c128a5d
Gordon Yeh <gordon.yeh.dev@gmail.com>
2020-09-09 00:05:42 -0700

    fix error when input and output are 0 bytes

--- src/app/pipes/bytes.pipe.ts
+++ src/app/pipes/bytes.pipe.ts
@@ -14,12 +14,16 @@ var denom = {
 export class BytesPipe implements PipeTransform {
   transform(value: number, unit: FileSizeUnit = null, numDecimals=2): unknown {
     if (unit === null) {
-      let maxUnit = 0;
-      for (const u in denom) {
-        if (value > (denom[u] / 2) && denom[u] > maxUnit) {
-          unit = u as FileSizeUnit;
-          maxUnit = denom[u];
+      if (value > 1) {
+        let maxUnit = 0;
+        for (const u in denom) {
+          if (value > (denom[u] / 2) && denom[u] > maxUnit) {
+            unit = u as FileSizeUnit;
+            maxUnit = denom[u];
+          }
         }
+      } else {
+        unit = 'B';
       }
     }
     let sizeInUnit = value / denom[unit];
