44e254d09cef3d94432d5a7abe4d1dcb2c9a31a0
Gordon Yeh <gordon.yeh.dev@gmail.com>
2020-09-08 22:22:30 -0700

    add variable file size unit display

--- src/app/pipes/bytes.pipe.ts
+++ src/app/pipes/bytes.pipe.ts
@@ -2,17 +2,26 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 type FileSizeUnit = 'B' | 'KB' | 'MB' | 'GB';
 var denom = {
-  'B': 1,
-  'KB': 1_000,
+  'GB': 1_000_000_000,
   'MB': 1_000_000,
-  'GB': 1_000_000_000
+  'KB': 1_000,
+  'B': 1
 };
 
 @Pipe({
   name: 'bytes'
 })
 export class BytesPipe implements PipeTransform {
-  transform(value: number, unit: FileSizeUnit, numDecimals=2): unknown {
+  transform(value: number, unit: FileSizeUnit = null, numDecimals=2): unknown {
+    if (unit === null) {
+      let maxUnit = 0;
+      for (const u in denom) {
+        if (value > (denom[u] / 2) && denom[u] > maxUnit) {
+          unit = u as FileSizeUnit;
+          maxUnit = denom[u];
+        }
+      }
+    }
     let sizeInUnit = value / denom[unit];
     return `${sizeInUnit.toFixed(numDecimals)} ${unit}`;
   }
